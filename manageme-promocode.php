b8f124e24defeae9ac2faaa2e7b1e3cd0ece5b9b
Emmanuel Piguet <man@wonderweb.ch>
2024-08-23 15:56:27 +0200

    fix wp error

--- manageme-promocode.php
+++ manageme-promocode.php
@@ -1,9 +1,8 @@
 <?php
 
 /**
- * @link              wonderweb.ch
+ * @see              wonderweb.ch
  * @since             1.0.0
- * @package           ManageMe_PromoCode
  *
  * @wordpress-plugin
  * Plugin Name:       ManageMe Promo Code
@@ -14,34 +13,39 @@
  * Author URI:        wonderweb.ch
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
-
  */
 
 // If this file is called directly, abort.
-if (! defined('WPINC')) {
-    die;
+if (!defined('WPINC')) {
+    exit;
 }
 
 define('MANAGEME_PROMOCODE_VERSION', '1.0.4');
 
-require plugin_dir_path(__FILE__) . 'inc/manageme-promocode-public.php';
+require plugin_dir_path(__FILE__).'inc/manageme-promocode-public.php';
 
 if (is_admin()) {
     // require_once plugin_dir_path( __FILE__ ) .'inc/manageme-slider-api-admin.php';
 }
-
 
 /**
  * Begins execution of the plugin.
  *
  * @since    1.0.0
  */
-
 function run_manageme_promo_api()
 {
     add_shortcode('manageme_promocode', 'display_promocode');
-    wp_enqueue_script('manageme-promo-api-js', plugin_dir_url(__FILE__) . 'js/manageme-promo.js', array( 'jquery' ), MANAGEME_PROMOCODE_VERSION, false);
-    wp_enqueue_style('manageme-promo-api-css', plugin_dir_url(__FILE__) . 'css/manageme-promo.css', array(), MANAGEME_PROMOCODE_VERSION, 'all');
-
+    add_action('wp_enqueue_scripts', 'enqueue_manageme_promo_assets');
 }
+
+/**
+ * Enqueue les scripts et styles du plugin.
+ */
+function enqueue_manageme_promo_assets()
+{
+    wp_enqueue_script('manageme-promo-api-js', plugin_dir_url(__FILE__).'js/manageme-promo.js', ['jquery'], MANAGEME_PROMOCODE_VERSION, false);
+    wp_enqueue_style('manageme-promo-api-css', plugin_dir_url(__FILE__).'css/manageme-promo.css', [], MANAGEME_PROMOCODE_VERSION, 'all');
+}
+
 run_manageme_promo_api();
