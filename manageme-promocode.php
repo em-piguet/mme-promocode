12c7177a852cd0e187a8213df674a165c1cc73e1
Emmanuel Piguet <man@wonderweb.ch>
2024-08-23 16:11:51 +0200

    clean code for test

--- manageme-promocode.php
+++ manageme-promocode.php
@@ -8,7 +8,7 @@
  * Plugin Name:       ManageMe Promo Code
  * Plugin URI:        manage-me.pro
  * Description:       Shortcode pour le code Promo -> [manageme_promocode societyid="XXX"]
- * Version:           1.0.6
+ * Version:           1.0.61
  * Author:            Wonderweb
  * Author URI:        wonderweb.ch
  * License:           GPL-2.0+
@@ -21,13 +21,9 @@ if (!defined('WPINC')) {
     exit;
 }
 
-define('MANAGEME_PROMOCODE_VERSION', '1.0.6');
+define('MANAGEME_PROMOCODE_VERSION', '1.0.61');
 
 require plugin_dir_path(__FILE__).'inc/manageme-promocode-public.php';
-
-if (is_admin()) {
-    // require_once plugin_dir_path( __FILE__ ) .'inc/manageme-slider-api-admin.php';
-}
 
 /**
  * Begins execution of the plugin.
